f3a60ccbfcd494889f86f3e42d63415d8638d0ff
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-09-15 21:13:36 -0500

    Finishing the first part, three of the test running

---
+++ src/main/java/org/adaschool/tdd/service/MongoWeatherService.java
@@ -0,0 +1,54 @@
+package org.adaschool.tdd.service;
+
+import org.adaschool.tdd.controller.weather.dto.WeatherReportDto;
+import org.adaschool.tdd.exception.WeatherReportNotFoundException;
+import org.adaschool.tdd.repository.WeatherReportRepository;
+import org.adaschool.tdd.repository.document.GeoLocation;
+import org.adaschool.tdd.repository.document.WeatherReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MongoWeatherService
+    implements WeatherService
+{
+
+    private final WeatherReportRepository repository;
+
+    public MongoWeatherService( @Autowired WeatherReportRepository repository )
+    {
+        this.repository = repository;
+    }
+
+    @Override
+    public WeatherReport report( WeatherReportDto weatherReportDto )
+    {
+        WeatherReport weatherReport = new WeatherReport(weatherReportDto);
+        repository.save(weatherReport);
+        return weatherReport;
+
+    }
+
+    @Override
+    public WeatherReport findById( String id )
+    {
+        if(repository.existsById(id)){
+            return repository.findById(id).orElse(null);
+        }
+        throw new WeatherReportNotFoundException();
+    }
+
+    @Override
+    public List<WeatherReport> findNearLocation( GeoLocation geoLocation, float distanceRangeInMeters )
+    {
+        return null;
+    }
+
+    @Override
+    public List<WeatherReport> findWeatherReportsByName( String reporter )
+    {
+        return null;
+    }
+}
