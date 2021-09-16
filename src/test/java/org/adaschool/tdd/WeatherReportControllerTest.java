e8b8664a4061fe14e285b655ae65733507b7470c
KyrieLW <james.weisnner@mail.escuelaing.edu.co>
2021-09-16 00:56:53 -0500

    Starting last part, creating WeatherReport Endpoint testing

--- src/test/java/org/adaschool/tdd/WeatherReportControllerTest.java
+++ src/test/java/org/adaschool/tdd/WeatherReportControllerTest.java
@@ -1,29 +1,26 @@
 package org.adaschool.tdd;
 
+import org.adaschool.tdd.service.MongoWeatherService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
-public class HealthControllerTest
-{
+public class WeatherReportControllerTest {
     @LocalServerPort
     private int port;
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void greetingShouldReturnDefaultMessage()
-        throws Exception
-    {
+    @Mock
+    private MongoWeatherService mongoWeatherService;
 
-        assertThat(
-            this.restTemplate.getForObject( "http://localhost:" + port + "/v1/health", String.class ) ).contains(
-            "API Working OK!" );
+    @Test
+    public void test(){
+
     }
 }
